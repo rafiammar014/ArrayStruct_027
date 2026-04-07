88f9f8a0205b1f5bed15ed8dd0177e6f2bc6d2cc
rafiammar014 <rafiammar014@gmail.com>
2026-04-07 14:50:46 +0700

    Add output statement to display third element of uninitialized array

--- Array.cpp
+++ Array.cpp
@@ -7,4 +7,9 @@ int angka[5] = {10, 20, 30, 40, 50};
 //deklarasi tanpa inisialisasi
 int datamu[5];
 int x;
+//cara membaca array dataku
+cout << "Data ke - 3 : " << dataku[2] << endl;
+
+//Operasi Matematika 
+x = dataku[3]
 }
