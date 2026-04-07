792b4b2f8ef3322ae7e88d790b8fb06326d67b90
rafiammar014 <rafiammar014@gmail.com>
2026-04-07 15:11:00 +0700

    Add struct definitions for DeatailAlamat and orang with relevant fields

---
+++ StructStruct.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Array.h"
+
+struct DeatailAlamat{
+    string desa;
+    string kota;
+};
+struct orang{
+    string nama;
+    DeatailAlamat alamat;
+    int umur;
+};
