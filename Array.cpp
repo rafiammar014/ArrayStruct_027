74a4bf625839d0342f68d1d5403c558b48e463ec
rafiammar014 <rafiammar014@gmail.com>
2026-04-07 15:04:30 +0700

    Fix variable name from dataku to datamu in array operations and output

--- Array.cpp
+++ Array.cpp
@@ -8,32 +8,32 @@ int angka[5] = {10, 20, 30, 40, 50};
 int datamu[5];
 int x;
 //cara membaca array dataku
-cout << "Data ke - 3 : " << dataku[2] << endl;
+cout << "Data ke - 3 : " << datamu[2] << endl;
 
 //Operasi Matematika 
-x = dataku[2]*3;
+x = datamu[2]*3;
 //cara menulis ata mengisi array
-dataku[2] = 600;
-cout << "Data ke - 3 yang baru : " << dataku[2] << endl;
+datamu[2] = 600;
+cout << "Data ke - 3 yang baru : " << datamu[2] << endl;
 
 //mengisi array datamu satu per satu
 cout << "Data Index ke - 0 : ";
-cin >> dataku[0];
+cin >> datamu[0];
 cout << "Data Index ke - 1 : "; 
-cin >> dataku[1];
+cin >> datamu[1];
 cout << "Data Index ke - 2 : ";
-cin >> dataku[2];
+cin >> datamu[2];
 cout << "Data Index ke - 3 : ";
-cin >> dataku[3];
+cin >> datamu[3];
 cout << "Data Index ke - 4 : ";
-cin >> dataku[4];
+cin >> datamu[4];
 
 //Tampilkan array datamu satu per satu 
-cout << "Data ke - 1 : " << dataku[0] << endl;
-cout << "Data ke - 2 : " << dataku[1] << endl;
-cout << "Data ke - 3 : " << dataku[2] << endl;
-cout << "Data ke - 4 : " << dataku[3] << endl;
-cout << "Data ke - 5 : " << dataku[4] << endl;
+cout << "Data ke - 1 : " << datamu[0] << endl;
+cout << "Data ke - 2 : " << datamu[1] << endl;
+cout << "Data ke - 3 : " << datamu[2] << endl;
+cout << "Data ke - 4 : " << datamu[3] << endl;
+cout << "Data ke - 5 : " << datamu[4] << endl;
 
 cout << endl;
 
@@ -45,3 +45,10 @@ for (int i = 0; i <= 4; i++)
 }
 
 cout << endl;
+//Menampilkan data mu dengan pengulangan for
+for (int i = 0; i <= 4; i++)
+{
+    cout << "Data ke - "<< i+1 << " : " << datamu[i] << endl;
+}
+
+}
